6e22e8764e0733b78b32e964d8edc391b7623a01
Harshit Singh <singh.harshit2561@gmail.com>
2025-09-14 14:28:35 +0530

    Remove old files

--- config.py
+++
@@ -1,9 +0,0 @@
-DB_CONFIG = {
-    'host': 'localhost',
-    'port': '5432',
-    'user': 'postgres',
-    'password': 'your_password_here',
-    'dbname': 'weatherdb'
-}
-
-CITIES_CSV_PATH = "data/indian_cities.csv"
